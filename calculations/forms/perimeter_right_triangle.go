1805a880018e11099947847120cab009a79d7e70
fromage222 <louispoller.lp@gmail.com>
2024-10-18 13:40:57 +0200

    .

--- calculations/forms/perimeter_right_triangle.go
+++ calculations/forms/perimeter_right_triangle.go
@@ -1,8 +1,10 @@
 package forms
 
+import "math"
+
 // Erwartet zwei Seitenlängen a und b.
 // Liefert den Umfang eines rechtwinkligen Dreiecks, dessen Katheten a und b sind.
 func PerimeterRightTriangle(a, b float64) float64 {
 	// TODO
-	return 0
+	return a + b + math.Sqrt(a*a+b*b)
 }
