291b19da228da627de6a10bd8f44bad8dec131d7
fromage222 <louispoller.lp@gmail.com>
2024-12-03 11:42:09 +0100

    .

--- string_lists/check_ordering/in_order.go
+++ string_lists/check_ordering/in_order.go
@@ -4,7 +4,20 @@ package check_ordering
 // Überprüft, ob die beiden Strings in der Liste in der gegebenen Reihenfolge vorkommen.
 // Gibt `true` zurück, wenn das der Fall ist, ansonsten `false`.
 func CheckOrdering(strings []string, first, second string) bool {
+	v1 := 0
+	v2 := 1
+	for i := 0; i < len(strings); i++ {
+		if strings[i] == first {
+			v1 += i
+		}
+		if strings[i] == second {
+			v2 += v2 + i
+		}
 
+	}
+	if v2 > v1 && v2 != 1 {
+		return true
+	}
 	return false
 }
 
--- string_lists/count/count.go
+++ string_lists/count/count.go
@@ -3,12 +3,11 @@ package count
 // Erwartet eine Liste von Strings sowie einen String, der gezählt werden soll.
 // Liefer die Anzahl der Vorkommen des gesuchten Strings in der Liste.
 func Count(strings []string, search string) int {
-	gefunden := 0
+	zaehler := 0
 	for i := 0; i < len(strings); i++ {
 		if strings[i] == search {
-
-			gefunden++
+			zaehler++
 		}
 	}
-	return gefunden
+	return zaehler
 }
